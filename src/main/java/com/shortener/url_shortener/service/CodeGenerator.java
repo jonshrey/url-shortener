45344efa1687bc31d38a7c47c8773591029419a8
Shreyansh <yourname@example.com>
2026-07-15 19:10:04 +0530

    feat : implemented base62 code generator

--- src/main/java/com/shortener/url_shortener/service/CodeGenerator.java
+++ src/main/java/com/shortener/url_shortener/service/CodeGenerator.java
@@ -1,10 +1,19 @@
 package com.shortener.url_shortener.service;
 
+import java.security.SecureRandom;
+
 public class CodeGenerator {
 
+    private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private static final SecureRandom RANDOM = new SecureRandom();
+
     public static String generate(int length) {
-        // Implementation for generating a random alphanumeric code
-        return null;
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            int index = RANDOM.nextInt(ALPHABET.length());
+            sb.append(ALPHABET.charAt(index));
+        }
+        return sb.toString();
     }
 
 }
